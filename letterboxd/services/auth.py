03e7d01cb67169a714ea97d4a4bd205e0aa9b3f9
Mark Boszko <mboszko@mac.com>
2018-06-13 15:03:50 -0700

    starting the auth service

---
+++ letterboxd/services/auth.py
@@ -0,0 +1,23 @@
+"""
+Authentication services for the Letterboxd API
+"""
+
+def token(username, password):
+
+
+##### The Ruby implementation:
+#
+# module Auth
+#
+#   attr_reader :token
+#
+#   def login username, password
+#     login_response = api "auth/token", method: "post", form: [["grant_type", "password"], ["username", username], ["password", password]]
+#     # login_response = api "auth/token", method: "post", params: {grant_type: "password", username: username, password: password}
+#     json = JSON.parse(login_response.to_s)
+#     @token = json["access_token"]
+#     raise "Unable to log in" unless @token
+#     login_response
+#   end
+#
+# end
