5142c36409da74fc77ae6d89492a267469b28b07
Mark Boszko <mboszko@mac.com>
2018-06-13 16:58:19 -0700

    Move initialization into the Letterboxd class

--- letterboxd/letterboxd.py
+++ letterboxd/letterboxd.py
@@ -3,6 +3,8 @@ Python 3 wrapper for
 Version 0 of the Letterboxd API
 """
 
+import os
+import requests
 # import api
 # import service/auth
 # import service/comments
@@ -14,12 +16,47 @@ from .services.film import Film
 # import service/members
 # import service/search
 
-
-LBX_API_SECRET = os.environ.get('LBX_API_SECRET', None)
+# default API URL for v0
 API_BASE_URL = "https://api.letterboxd.com/api/v0"
 
+
 class Letterboxd(object):
-    def __init__(self, api_base, api_key, api_secret):
+    def __init__(self, api_base=API_BASE_URL, api_key="", api_secret=""):
         self.api_base = api_base
         self.api_key = api_key
         self.api_secret = api_secret
+
+        if (self.api_key == ""):
+            # If the variable wasn't passed in,
+            # try to get the environment variable instead
+            API_KEY = os.environ.get('LBX_API_KEY', None)
+
+            class APIKeyMissingError(Exception):
+                pass
+
+            if API_KEY is None:
+                raise APIKeyMissingError(
+                        "All methods require an API key. See "
+                        "https://letterboxd.com/api-coming-soon/ "
+                        "for more information"
+                )
+
+        if (self.api_secret == ""):
+            # If the variable wasn't passed in,
+            # try to get the environment variable instead
+            API_SECRET = os.environ.get('LBX_API_SECRET', None)
+
+            class APISecretMissingError(Exception):
+                pass
+
+            if API_SECRET is None:
+                raise APISecretMissingError(
+                        "All methods require an API secret. See "
+                        "https://letterboxd.com/api-coming-soon/ "
+                        "for more information"
+                )
+
+        # Start the shared requests session
+        self.session = requests.Session()
+        self.session.params = {}
+        self.session.params['api_key'] = API_KEY
