2b5b87ab953439417c272a069d6ecf1c36b50252
Mark Boszko <mboszko@mac.com>
2018-06-13 17:14:28 -0700

    Instantiate using environment variables

--- examples.py
+++ examples.py
@@ -2,13 +2,14 @@ import json
 from .letterboxd.letterboxd import Letterboxd
 import helpers
 
+# Assuming use of environment variables:
+lbxd = Letterboxd()
 
-API_KEY = 'YOUR_KEY_HERE'
-API_SECRET = 'YOUR_SECRET_HERE'
+# If not using environment variables, instead instantiate Letterboxd() with your secrets:
+# API_KEY = 'YOUR_KEY_HERE'
+# API_SECRET = 'YOUR_SECRET_HERE'
+# lbxd = Letterboxd(api_key=API_KEY, api_secret=API_SECRET)
 
-API_BASE = 'https://api.letterboxd.com/api/v0'
-
-lbxd = Letterboxd.new API_BASE, API_KEY, API_SECRET
-
-lbxd.login 'YOUR_USERNAME_HERE', 'YOUR_PASSWORD_HERE'
-helpers.show_json(lbxd.get_me)
+# FIXME: ruby implementation
+# lbxd.login 'YOUR_USERNAME_HERE', 'YOUR_PASSWORD_HERE'
+# helpers.show_json(lbxd.get_me)
