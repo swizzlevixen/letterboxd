6aa84a39b23de0b7434965b065fbd1e4e4918538
Mark Boszko <mboszko@mac.com>
2018-06-18 13:16:41 -0700

    Updating the test for the new methods

--- tests/test_letterboxd.py
+++ tests/test_letterboxd.py
@@ -1,44 +1,38 @@
 from pytest import fixture
 from letterboxd.letterboxd import Letterboxd
 import logging
+import requests
 import vcr
 
 logging.getLogger(__name__)
-FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-logging.basicConfig(format=FORMAT, level = logging.DEBUG)
+# FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+# logging.basicConfig(format=FORMAT, level = logging.DEBUG)
+logging.basicConfig(level = logging.DEBUG)
 
 @fixture
 def film_keys():
     # Responsible only for returning the test data
-    return ['id', 'name', 'originalName', 'alternativeNames',
+    # A film could also include 'originalName', but does not apply here
+    return ['id', 'name', 'alternativeNames',
             'releaseYear', 'tagline', 'description', 'runTime',
             'poster', 'backdrop', 'backdropFocalPoint', 'trailer',
             'genres', 'contributions', 'filmCollectionId', 'links']
 
-# @vcr.use_cassette('tests/vcr_cassettes/film_info.yml')
-# def test_film_info(film_keys):
-#     """Tests API call to get a film's info"""
-#
-#     lbxd = Letterboxd()
-#
-#     film_instance = Film("raiders-of-the-lost-ark")
-#     response = film_instance.info()
-#
-#     assert isinstance(response, dict)
-#     assert response['id'] == "raiders-of-the-lost-ark", "The ID should be in the response"
-#     assert set(film_keys).issubset(response.keys()), "All keys should be in the response"
-
-@vcr.use_cassette('tests/vcr_cassettes/movie-info.yml', filter_query_parameters=['api_key', 'api_secret'])
+#@vcr.use_cassette('tests/vcr_cassettes/movie-info.yml', filter_query_parameters=['api_key', 'api_secret'])
 def test_film_info():
     """Tests API call to get a film's info"""
 
-    # Assume use of environment variables
+    # Assume use of environment variables for api key and secret
     lbxd = Letterboxd()
 
     film_instance = lbxd.film(film_id = "2bbs")  # Raiders of the Lost Ark
     response = film_instance.info()
     logging.debug("response: {}".format(response))
+    assert isinstance(response, requests.Response)
+    assert response.status_code == 200
 
-    assert isinstance(response, dict)
-    assert response['id'] == "2bbs", "The ID should be in the response"
-    assert set(film_keys).issubset(response.keys()), "All keys should be in the response"
+    response_json = response.json()
+    logging.debug("response_json: {}".format(response_json))
+    assert isinstance(response_json, dict)
+    assert response_json['id'] == "2bbs", "The ID should be in the response"
+    assert set(film_keys()).issubset(response_json.keys()), "All keys should be in the response"
