c4381f05a37ed1383da6d8540de07e2891e6b544
Mark Boszko <mboszko@mac.com>
2018-06-21 12:52:54 -0700

    trying to move from `login()` to `get_token()` using requests-oauthlib

--- letterboxd/services/auth.py
+++ letterboxd/services/auth.py
@@ -5,11 +5,15 @@ Authentication API Documentation:
 http://api-docs.letterbotokend.com/#auth
 """
 
+import json
 import logging
 import requests
-from requests_oauthlib import oauth2_auth
+from oauthlib.oauth2 import LegacyApplicationClient
+from requests_oauthlib import OAuth2Session
 
 logging.getLogger(__name__)
+
+# TODO: This token business should just take care of itself. Instantiate with user & pass, call token, and if there isn't a token already. If it's about to expire, renew it; if it's expierd, get a new token. The other code should just ask for the token, and this auth takes care of the rest.
 
 
 class Authentication:
@@ -36,12 +40,34 @@ class Authentication:
         logging.debug("deleter of token called")
         del self._token
 
-    def login(self, username, password):
-        form = {"grant_type": "password", "username": username, "password": password}
-        login_response = self._api.api_call(path="auth/token", method="post", form=form)
-        login_response_json = login_response.json()
-        self.token = login_response_json["access_token"]
-        if not token:
-            # TODO: There's probably a JSON response error we can display instead
-            raise ConnectionRefusedError("No token received")
-        return login_response
+    # Goal: Removing this in favor of `get_token`
+    # def login(self, username, password):
+    #     form = [
+    #         ["grant_type", "password"],
+    #         ["username", username],
+    #         ["password", password],
+    #     ]
+    #     logging.debug("form: {}".format(form))
+    #     login_response = self._api.api_call(path="auth/token", method="post", form=form)
+    #     login_response_json = login_response.json()
+    #     self.token = login_response_json["access_token"]
+    #     if not token:
+    #         # TODO: There's probably a JSON response error we can display instead
+    #         raise ConnectionRefusedError("No token received")
+    #     return self.token
+
+    def get_token(self, username, password):
+        client = LegacyApplicationClient(client_id=self._api.api_key)
+        oauth = OAuth2Session(client=client)
+        token_url = "{}/auth/token".format(self._api.api_base)
+        # FIXME: I believe I need to sign this request and add it as a header, as:
+        # headers["Authorization"] = "Signature {}".format(signature)\
+        # How can I get the prepared_request for this token request?
+        token = oauth.fetch_token(
+            token_url=token_url,
+            username=username,
+            password=password,
+            client_id=self._api.api_key,
+            client_secret=self._api.api_secret,
+        )
+        return self.token
