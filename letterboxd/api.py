d3926bb97814b74fb7ccdf3f7ccf399bcdbab43d
Mark Boszko <mboszko@mac.com>
2018-06-21 21:46:39 -0700

    I was, in fact, url-encoding the data twice

--- letterboxd/api.py
+++ letterboxd/api.py
@@ -84,10 +84,8 @@ class API:
 
         if form:
             logging.debug("API.api_call() if form")
-            # Is there any other need to use `form` than for an oAuth call?
+            # TODO: Is there any other need to use `form` than for an oAuth call?
             # should some of this code be in there instead?
-            # Escape the form string
-            body = urllib.parse.quote_plus(form)
             headers["Content-Type"] = "application/x-www-form-urlencoded"
             # Prepare the request
             prepared_dict = self.__prepare_request(
