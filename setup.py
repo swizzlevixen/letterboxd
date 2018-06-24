764d5bc46112d1ac8874f4abed600fe4857c13f8
Mark Boszko <mboszko@mac.com>
2018-06-24 16:36:40 -0700

    Preparing setup.py for 0.1.0 release

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
-setup(name='letterboxd',
-      version='0.1',
-      description='Python wrapper for Letterboxd API',
-      url='http://github.com/bobtiki/letterboxd',
-      author='Mark Boszko',
-      author_email='mboszko@mac.com',
-      license='MIT',
-      packages=['letterboxd'],
-      zip_safe=False)
+setup(
+    name="letterboxd",
+    packages=["letterboxd, letterboxd.services"],
+    version="0.1.0",
+    description="Python 3.6+ wrapper for Letterboxd API",
+    author="Mark Boszko",
+    author_email="mboszko@mac.com",
+    copyright="Copyright (c) 2018 Mark Boszko",
+    url="http://github.com/bobtiki/letterboxd",
+    download_url="",
+    keywords=["api", "movies"],
+    classifiers=["Programming Language :: Python :: 3"],
+    license="MIT",
+)
