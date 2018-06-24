6896d7531b64aa782eee33f14f86bab17bb926e2
Mark Boszko <mboszko@mac.com>
2018-06-24 14:15:03 -0700

    More thorough test

--- tests/test_member.py
+++ tests/test_member.py
@@ -2,6 +2,7 @@
 import logging
 
 from letterboxd.letterboxd import Letterboxd
+from letterboxd.services.member import Member
 from tests.test_letterboxd import load_user_pass
 
 
@@ -12,8 +13,15 @@ def test_member_watchlist():
     test_user = lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
     # get the watchlist
     member_id = test_user.me["member"]["id"]
+    assert isinstance(member_id, str)
     member = lbxd.member(member_id=member_id)
+    assert isinstance(member, Member)
     watchlist_response = member.watchlist()
+    assert isinstance(response, requests.Response)
+    assert response.status_code == 200
     watchlist = watchlist_response.json
-    logging.debug("me_dict: {}".format(me_dict))
-    assert isinstance(me_dict, dict)
+    logging.debug("me_dict: {}".format(watchlist))
+    assert isinstance(watchlist, dict)
+    # assert set(watchlist_keys()).issubset(
+    #         watchlist.keys()
+    # ), "All keys should be in the response"
