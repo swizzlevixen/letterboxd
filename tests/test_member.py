0babf0b1692f611eb641f13537d99ad0c46eea1a
Mark Boszko <mboszko@mac.com>
2018-06-24 13:52:36 -0700

    TDD for member/{id}/watchlist

---
+++ tests/test_member.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+import logging
+
+from letterboxd.letterboxd import Letterboxd
+from tests.test_letterboxd import load_user_pass
+
+
+def test_member_watchlist():
+    # set up
+    LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
+    lbxd = Letterboxd()
+    test_user = lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
+    # get the watchlist
+    member_id = test_user.me["member"]["id"]
+    watchlist_response = lbxd.member.watchlist(member_id=member_id)
+    watchlist = watchlist_response.json
+    logging.debug("me_dict: {}".format(me_dict))
+    assert isinstance(me_dict, dict)
