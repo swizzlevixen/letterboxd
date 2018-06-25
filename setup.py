c1caa51dbf81cc3ee47d77e9d8d759de62612ba6
Mark Boszko <mboszko@mac.com>
2018-06-24 19:28:45 -0700

    Working on setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(
+# Read in the README file, for the long_description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
     name="letterboxd",
-    packages=["letterboxd, letterboxd.services"],
+    packages=["letterboxd", "letterboxd.services", "tests"],
     version="0.1.0",
-    description="Python 3.6+ wrapper for Letterboxd API",
+    description="Python 3.6+ wrapper for the Letterboxd API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Mark Boszko",
     author_email="mboszko@mac.com",
     copyright="Copyright (c) 2018 Mark Boszko",
