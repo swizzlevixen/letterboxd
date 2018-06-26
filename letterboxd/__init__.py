126047e6366fba848bcbe2104852c2faab8237b6
Mark Boszko <mboszko@mac.com>
2018-06-26 13:46:37 -0700

    Fixes #10 added a method for letterboxd.new()

--- letterboxd/__init__.py
+++ letterboxd/__init__.py
@@ -1,5 +1,12 @@
 import logging
+from letterboxd.config import *
+from letterboxd.letterboxd import Letterboxd
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 name = "letterboxd"
+
+
+def new(api_base=API_BASE_URL, api_key="", api_secret=""):
+    lbxd = Letterboxd(api_base=api_base, api_key=api_key, api_secret=api_secret)
+    return lbxd
