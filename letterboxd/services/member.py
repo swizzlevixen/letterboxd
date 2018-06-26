d0264799474cdaa0a25426b7fb333268f148da8e
Mark Boszko <mboszko@mac.com>
2018-06-25 20:00:02 -0700

    Better docstrings

--- letterboxd/services/member.py
+++ letterboxd/services/member.py
@@ -1,30 +1,39 @@
-"""
-Member(s) services for the Letterboxd API
-
-/member endpoints documentation
-http://api-docs.letterboxd.com/#path--member--id-
-
-/members endpoints documentation
-http://api-docs.letterboxd.com/#path--members
-"""
 import logging
 
 logging.getLogger(__name__)
 
 
 class Member(object):
+    """
+    /member services for the Letterboxd API
+    """
+
     def __init__(self, api, member_id=None):
+        """
+
+        :param api: API object
+        :param member_id: str - the LID of the member
+        """
         self._api = api
         self._member_id = member_id
 
     def details(self, member_id=None):
+        """
+        /member
+        http://api-docs.letterboxd.com/#path--member--id-
+        TODO: Write this function
+
+        :param member_id: str - member LID
+        :return:
+        """
         pass
 
     def watchlist(self, member_id=None, watchlist_request={}):
         """
         /member/{id}/watchlist
         http://api-docs.letterboxd.com/#path--member--id--watchlist
-        :param member_id: str - LID of member to compare to?
+
+        :param member_id: str - LID of member to get
         :param watchlist_request: dict - WatchlistRequest
         :return: dict - FilmsResponse
         """
