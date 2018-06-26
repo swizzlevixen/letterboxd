4c40f9259d44c3be55a02cb80c106a643ff16823
Mark Boszko <mboszko@mac.com>
2018-06-26 13:29:42 -0700

    Configuration file for constants

---
+++ letterboxd/config.py
@@ -0,0 +1,11 @@
+"""
+Configuration
+"""
+
+# -------------------------
+# API Base URL
+# -------------------------
+# default API URL for v0
+API_BASE_URL = "https://api.letterboxd.com/api/v0"
+
+# TODO: Move the LBXD_API_KEY and LBXD_API_KEY in here?
