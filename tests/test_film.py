256b3ded434a839016d55202bb648f52ea873e71
Mark Boszko <mboszko@mac.com>
2018-06-26 23:31:07 -0700

    Implemented Film.report()

--- tests/test_film.py
+++ tests/test_film.py
@@ -61,3 +61,20 @@ def test_film_member():
     )
     logging.debug(f"response_json: {member_film_relationships_response}")
     assert isinstance(member_film_relationships_response, dict)
+
+
+def test_film_report():
+    LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
+    lbxd = Letterboxd()
+    # login, even though we don't use this value
+    test_user = lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
+    film_instance = lbxd.film(film_id="9mMS")  # Kirk Cameron’s Saving Christmas
+    report_film_request = {
+        "reason": "Other",
+        "message": "Please disregard. Testing the API call.",
+    }
+    response_status_code = film_instance.report(
+        film_id="9mMS", report_film_request=report_film_request
+    )
+    logging.debug(f"response_status_code: {response_status_code}")
+    assert response_status_code is 204
