c7163aeea8b8c506cd8b81414116b2f0fa6f8dce
Mark Boszko <mboszko@mac.com>
2018-06-30 09:26:34 -0700

    Make the /films method a proper class method.

--- tests/test_film.py
+++ tests/test_film.py
@@ -66,23 +66,18 @@ def test_film_member():
     # TODO: test returned keys
 
 
-# Disabled report() test because this returns a 405 Method Not Allowed status code
-#
-# def test_film_report():
-#     LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
-#     lbxd = Letterboxd()
-#     # login, even though we don't use this value
-#     test_user = lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
-#     film_instance = lbxd.film(film_id="9mMS")  # Kirk Cameron’s Saving Christmas
-#     report_film_request = {
-#         "reason": "Other",
-#         "message": "Please disregard. Testing the API call.",
-#     }
-#     response_status_code = film_instance.report(
-#         film_id="9mMS", report_film_request=report_film_request
-#     )
-#     logging.debug(f"response_status_code: {response_status_code}")
-#     assert response_status_code is 204
+def test_film_report():
+    LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
+    lbxd = Letterboxd()
+    # login, even though we don't use this value
+    test_user = lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
+    film_instance = lbxd.film(film_id="9mMS")  # Kirk Cameron’s Saving Christmas
+    report_film_request = {"reason": "Other", "message": "TEST — IGNORE"}
+    response_status_code = film_instance.report(
+        film_id="9mMS", report_film_request=report_film_request
+    )
+    logging.debug(f"response_status_code: {response_status_code}")
+    assert response_status_code is 204
 
 
 def test_film_statistics():
