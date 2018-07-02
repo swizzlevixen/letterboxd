b79fe2c795dbeccc22167a2dea232fe9414f5ca0
Mark Boszko <mboszko@mac.com>
2018-07-01 18:22:24 -0700

    Testing user auth reaction to a bad user/pass

--- tests/test_user.py
+++ tests/test_user.py
@@ -18,6 +18,17 @@ def test_user_auth():
     assert lbxd.api.user.token is test_token
 
 
+def test_user_auth_bad():
+    LBXD_USERNAME = "Butts McGee"
+    LBXD_PASSWORD = "123456"
+    lbxd = Letterboxd()
+    # make login
+    try:
+        test_user = lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
+    except Exception as e:
+        logging.error(e.args)
+
+
 def test_user_me():
     LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
     lbxd = Letterboxd()
