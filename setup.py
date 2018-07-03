ab5c6c3e86ac6bf9532699f976a54a7cbd770a9b
Mark Boszko <mboszko@mac.com>
2018-07-02 17:32:12 -0700

    boilerplate to merge in

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import setuptools
 with open("README.markdown", "r") as fh:
     long_description = fh.read()
 
+with open("CHANGELOG.md") as history_file:
+    history = history_file.read()
+
 setuptools.setup(
     name="letterboxd",
     packages=setuptools.find_packages(exclude=["contrib", "docs", "tests*", "sample"]),
@@ -37,4 +40,59 @@ setuptools.setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     license="MIT",
+)
+
+
+# -----------------------------------
+# Boilerplate below: merge in.
+# -----------------------------------
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest"]
+
+setup(
+    author="Mark Boszko",
+    author_email="mboszko@mac.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    description="Python 3 wrapper for the Letterboxd API.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="letterboxd",
+    name="letterboxd",
+    packages=find_packages(include=["letterboxd"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/bobtiki/letterboxd",
+    version="0.1.0",
+    zip_safe=False,
 )
