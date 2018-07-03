7e21bc5f656190b225f78f8319a3e6e9cebf0222
Mark Boszko <mboszko@mac.com>
2018-07-02 21:05:12 -0700

    Better docstrings

--- letterboxd/services/member.py
+++ letterboxd/services/member.py
@@ -5,35 +5,46 @@ logging.getLogger(__name__)
 
 class Member(object):
     """
-    /member services for the Letterboxd API
+    /member/* services for the Letterboxd API
     """
 
     def __init__(self, api, member_id=None):
         """
+        Initializes a Member object with a specific member by LID.
 
         :param api: API object
-        :param member_id: str - the LID of the member
+        :param member_id: str - LID of the member
         """
         self._api = api
         self._member_id = member_id
 
     def details(self, member_id=None):
         """
-        /member
-        http://api-docs.letterboxd.com/#path--member--id-
-        TODO: Write this function
+        /member/{id}
 
-        :param member_id: str - member LID
-        :return:
+        Get details about a member by ID.
+
+        # TODO: Write this function
+
+        :param member_id: str - The LID of the member.
+        :return: dict - Member
         """
         pass
 
     def watchlist(self, member_id=None, watchlist_request={}):
         """
         /member/{id}/watchlist
-        http://api-docs.letterboxd.com/#path--member--id--watchlist
 
-        :param member_id: str - LID of member to get
+        Get details of a member’s public watchlist by ID.
+
+        The response will include the film relationships for the signed-in
+        member, the watchlist’s owner, and the member indicated by the member
+        LID if specified (the member and memberRelationship parameters are
+        optional, and can be used to perform comparisons between the watchlist
+        owner and another member). Use the /film/{id}/me endpoint to add or
+        remove films from a member’s watchlist.
+
+        :param member_id: str - The LID of the member.
         :param watchlist_request: dict - WatchlistRequest
         :return: dict - FilmsResponse
         """
