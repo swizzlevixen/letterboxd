61a860d16ab4080c339f9dfba98e0ae7782a1898
Mark Boszko <mboszko@mac.com>
2018-07-04 20:16:14 -0700

    move setup to Readme.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 import setuptools
 
 # Read in the README file, for the long_description.
-with open("README.md", "r") as readme_file:
+with open("README.rst", "r") as readme_file:
     readme = readme_file.read()
 
 with open("CHANGELOG.md") as changelog_file:
