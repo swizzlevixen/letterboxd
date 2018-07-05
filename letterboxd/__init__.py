7241790f82465497fbf1a30ee412e62b14011409
Mark Boszko <mboszko@mac.com>
2018-07-04 23:14:48 -0700

    Bump version: 0.2.4 → 0.2.5

--- letterboxd/__init__.py
+++ letterboxd/__init__.py
@@ -12,7 +12,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 name = "letterboxd"
 __author__ = """Mark Boszko"""
 __email__ = "mboszko@mac.com"
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 
 def new(api_base=API_BASE_URL, api_key="", api_secret=""):
