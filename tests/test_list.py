52a2b37cff5f727c3a9d8cf0a6162e3519db733c
Mark Boszko <mboszko@mac.com>
2018-07-07 15:42:04 -0700

    Testing create_list()
    Closes #29

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import datetime
 import logging
 import pprint
 
@@ -72,3 +73,49 @@ def test_lists():
     assert set(list_summary_keys()).issubset(
         list_summary.keys()
     ), "All keys should be in list_summary."
+
+
+def list_create_response_keys():
+    """Returns list of keys in ListCreateResponse"""
+    return ["data", "messages"]
+
+
+def test_create_list():
+    """Tests API call to get a film's details"""
+
+    # Assume use of environment variables for api key and secret
+    lbxd = letterboxd.new()
+    # Login as a user
+    LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
+    lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
+    list_creation_request = {
+        "published": True,
+        "name": f"Top Two Movies in this Test List {datetime.datetime.now().isoformat()}",
+        "ranked": True,
+        "description": "This is the description that I'm testing with.",
+        # "clonedFrom": ""
+        "tags": ["API", "test"],
+        "entries": [
+            {
+                "film": "2bbs",
+                "rank": 2,
+                "notes": "Here are the notes for Raiders",
+                "containsSpoilers": True,
+            },
+            {
+                "film": "23c8",
+                "rank": 1,
+                "notes": "This should be #1, KKBB",
+                "containsSpoilers": False,
+            },
+        ],  # ListCreateEntry
+        # "share": "Facebook"
+    }
+    list_create_response = lbxd.create_list(list_creation_request=list_creation_request)
+    assert isinstance(list_create_response, dict)
+    logging.debug("-------------------------\nlist_create_response:")
+    logging.debug(pprint.pformat(list_create_response))
+    logging.debug(f"list_create_response.keys() {list_create_response.keys()}")
+    assert set(list_create_response_keys()).issubset(
+        list_create_response.keys()
+    ), "All keys should be in the lists_response."
