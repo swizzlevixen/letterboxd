c23663609b51c07f679ee7bb4692ac9561517881
Mark Boszko <mboszko@mac.com>
2018-07-06 20:19:39 -0700

    Call the API from lists()

--- letterboxd/services/list.py
+++ letterboxd/services/list.py
@@ -4,5 +4,21 @@ logging.getLogger(__name__)
 
 
 class Lists(object):
-    def lists(self, lists_request):
-        return {}
+    """
+    /lists service for the Letterboxd API
+    """
+
+    def lists(self, lists_request={}):
+        """
+        /lists
+
+        A cursored window over a list of lists.
+
+        Use the ‘next’ cursor to move through the list.
+
+        :param lists_request: dict - ListsRequest
+        :return: dict - ListsResponse
+        """
+        response = self._api.api_call(path="lists", params=lists_request)
+        lists = response.json()
+        return lists
