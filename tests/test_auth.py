3262e02ba83f0761f43fb642352cfb763407916a
Mark Boszko <mboszko@mac.com>
2018-07-13 13:07:06 -0700

    calling the Authentication class instead of the instance.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,15 +1,12 @@
 #! /usr/bin/env python3
 
 import letterboxd
-from tests.test_letterboxd import load_user_pass
+from letterboxd.services.auth import Authentication
 
 
 def test_forgotten_password_request():
     lbxd = letterboxd.new()
-    # FIXME: Log in shouldn't be necessary for this
-    LBXD_USERNAME, LBXD_PASSWORD = load_user_pass()
-    lbxd.user(LBXD_USERNAME, LBXD_PASSWORD)
-    status_code = lbxd.auth.forgotten_password_request(
-        forgotten_password_request={"emailAddress": "user@example.com"}
+    status_code = Authentication.forgotten_password_request(
+        api=lbxd.api, forgotten_password_request={"emailAddress": "user@example.com"}
     )
-    assert status_code is 400
+    assert status_code is 204
