ed6d52e29417653db9576c07cfd7f4ddafb4db0e
Mark Boszko <mboszko@mac.com>
2018-07-13 13:18:38 -0700

    Starting TDD for username_check()

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+from random import randint
+
 import letterboxd
 from letterboxd.services.auth import Authentication
 
@@ -10,3 +12,13 @@ def test_forgotten_password_request():
         api=lbxd.api, forgotten_password_request={"emailAddress": "user@example.com"}
     )
     assert status_code is 204
+
+
+def test_username_check():
+    lbxd = letterboxd.new()
+    # Hopefully this random username is available; not actually guaranteed
+    sort_of_random_username = f"taco{randint(100, 999)}cat"
+    username_check_response = Authentication.username_check(
+        api=lbxd.api, username=sort_of_random_username
+    )
+    assert username_check_response["result"] is "Available"
