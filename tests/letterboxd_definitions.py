aab4394b04e956866bc1c113dd829e276d90b46c
Mark Boszko <mboszko@mac.com>
2018-07-19 18:37:15 -0700

    Moved Letterboxd API definitions to separate module
    And wrote a LOT more definitions

--- tests/letterboxd_definitions.py
+++ tests/letterboxd_definitions.py
@@ -24,14 +24,14 @@ def film_collection_keys():
 
 @fixture
 def film_keys():
-    # Film definition
-    # http://api-docs.letterboxd.com/#/definitions/Film
-    # Commented lines may not be returned by every film
+    """
+    Film definition
+
+    Optional keys: "originalName", "alternativeNames"
+    """
     return [
         "id",
         "name",
-        # "originalName",
-        # "alternativeNames",
         "releaseYear",
         "tagline",
         "description",
@@ -48,22 +48,91 @@ def film_keys():
 
 
 @fixture
-def film_summary_keys():
-    # FilmSummary definition
-    # http://api-docs.letterboxd.com/#/definitions/FilmSummary
-    # Commented lines may not be returned by every film
+def film_availability_response_keys():
+    """FilmAvailabilityResponse definition"""
+    return ["FilmAvailability"]
+
+
+@fixture
+def film_availability_keys():
+    """
+    FilmAvailability definition
+
+    Optional keys: "id"
+    """
+    return ["service", "displayName", "country", "url"]
+
+
+@fixture
+def film_relationship_keys():
+    """
+    FilmRelationship definition
+
+    Optional keys: "whenWatched", "whenLiked", "whenAddedToWatchlist",
+        "whenCompletedInWatchlist", "rating"
+    """
+    return ["watched", "liked", "favorited", "inWatchlist", "reviews", "diaryEntries"]
+
+
+@fixture
+def film_relationship_update_response_keys():
+    """FilmRelationshipUpdateResponse definition"""
+    return ["data", "messages"]
+
+
+@fixture
+def member_film_relationships_response_keys():
+    """
+    MemberFilmRelationshipsResponse definition
+
+    Optional keys: "next"
+    """
+    return ["items"]
+
+
+@fixture
+def member_film_relationship_keys():
+    """MemberFilmRelationship definition"""
+    return ["member", "relationship"]
+
+
+@fixture
+def member_summary_keys():
+    """
+    MemberSummary definition
+
+    Optional keys:  "givenName", "familyName"
+    """
     return [
         "id",
-        "name",
-        # "originalName",
-        # "alternativeNames",
-        "releaseYear",
-        "directors",
-        "poster",
-        # "filmCollectionId",
-        "links",
-        "relationships",
+        "username",
+        "displayName",
+        "shortName",
+        "pronoun",
+        "avatar",
+        "memberStatus",
     ]
+
+
+@fixture
+def film_statistics_keys():
+    """
+    FilmStatistics definition
+
+    Optional keys: "rating" (I suspect "ratingsHistogram" might be?)
+    """
+    return ["film", "counts", "ratingsHistogram"]
+
+
+@fixture
+def film_summary_keys():
+    """
+    FilmSummary definition
+
+    Optional keys: "originalName", "alternativeNames", "filmCollectionId",
+        "releaseYear", "poster",
+    """
+    return ["id", "name", "directors", "links", "relationships"]
 
 
 # -------------------------
@@ -73,10 +142,38 @@ def film_summary_keys():
 
 @fixture
 def films_response_keys():
-    # FilmsResponse definition
-    # http://api-docs.letterboxd.com/#/definitions/FilmsResponse
-    # Responsible only for returning the test data
+    """FilmsResponse definition"""
     return ["next", "items"]
+
+
+@fixture
+def film_services_response_keys():
+    """FilmServicesResponse definition"""
+    return ["items"]
+
+
+@fixture
+def service_keys():
+    """Service definition"""
+    return ["id", "name"]
+
+
+@fixture
+def genres_response_keys():
+    """GenresResponse definition"""
+    return ["items"]
+
+
+@fixture
+def genre_keys():
+    """Genre definition"""
+    return ["id", "name"]
+
+
+@fixture
+def link_keys():
+    """Link definition"""
+    return ["type", "id", "url"]
 
 
 # -------------------------
@@ -87,6 +184,6 @@ def films_response_keys():
 @fixture
 def member_settings_update_response_keys():
     """
-    MemberSettingsUpdateResponse keys definition
+    MemberSettingsUpdateResponse definition
     """
     return ["data", "messages"]
