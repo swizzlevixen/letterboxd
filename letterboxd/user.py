80df19ffa36fbfe0ac97b857c498aadc4887a421
Mark Boszko <mboszko@mac.com>
2018-07-29 00:07:52 -0700

    Improve documentation with type hints and reStructured Text docstrings

--- letterboxd/user.py
+++ letterboxd/user.py
@@ -9,17 +9,17 @@ logging.getLogger(__name__)
 
 
 class User(object):
-    """
-    Provices access token and shortcuts to user-focused methods
-    """
+    """Provices access token and shortcuts to user-focused methods"""
 
-    def __init__(self, api, username, password):
-        """
-        Set up authentication for this user.
+    def __init__(self, api, username: str, password: str) -> None:
+        """Set up authentication for this user.
 
-        :param api: Letterboxd.API - class instance
-        :param username: str
-        :param password: str
+        :param api: API class instance
+        :type api: Letterboxd.API
+        :param username: Letterboxd user name
+        :type username: str
+        :param password: Password
+        :type password: str
         """
         self._api = api
         self._auth = Authentication(api=api, username=username, password=password)
@@ -27,16 +27,16 @@ class User(object):
         self.token
 
     @property
-    def token(self):
-        """
-        Ask services.auth to get a token, and return the token string
+    def token(self) -> str:
+        """Ask services.auth to get a token, and return the token string
 
-        :return: str - oAuth token
+        :return: oAuth token
+        :rtype: str
         """
         return self._auth.token
 
     @property
-    def me(self):
+    def me(self) -> dict:
         """
         /me
 
@@ -45,14 +45,15 @@ class User(object):
         Calls to this endpoint must include the access token for an
         authenticated member.
 
-        :return: dict - MemberAccount
+        :return: MemberAccount
+        :rtype: dict
         """
         response = self._api.api_call(path="me")
         member_account = response.json()
         self._me = member_account
         return self._me
 
-    def me_update(self, member_settings_update_request):
+    def me_update(self, member_settings_update_request: dict) -> dict:
         """
         /me
 
@@ -60,8 +61,10 @@ class User(object):
 
         Calls to this endpoint must include the access token for an authenticated member
 
-        :param member_settings_update_request: dict - MemberSettingsUpdateRequest
-        :return: dict - MemberSettingsUpdateResponse
+        :param member_settings_update_request: MemberSettingsUpdateRequest
+        :type member_settings_update_request: dict
+        :return: MemberSettingsUpdateResponse
+        :rtype: dict
         """
         response = self._api.api_call(
             path="me", method="PATCH", params=member_settings_update_request
